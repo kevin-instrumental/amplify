5f6cb25b141bbd53c03b9d25f08b1ad880ac51be
Kevin Ikelle <kevin.ikelle@weareinstrumental.com>
2022-02-17 14:56:07 +0000

    pulled amplify files

---
+++ src/ui-components/LOGIN13.jsx
@@ -0,0 +1,359 @@
+/***************************************************************************
+ * The contents of this file were generated with Amplify Studio.           *
+ * Please refrain from making any modifications to this file.              *
+ * Any changes to this file will be overwritten when running amplify pull. *
+ **************************************************************************/
+
+/* eslint-disable */
+import React from "react";
+import { getOverrideProps } from "@aws-amplify/ui-react/internal";
+import { Image, Text, View } from "@aws-amplify/ui-react";
+export default function LOGIN13(props) {
+  const { overrides: overridesProp, ...rest } = props;
+  const overrides = { ...overridesProp };
+  return (
+    <View
+      width="1366px"
+      height="1007px"
+      overflow="hidden"
+      position="relative"
+      padding="0px 0px 0px 0px"
+      backgroundColor="rgba(255,200.00000327825546,30.00000011175871,1)"
+      {...rest}
+      {...getOverrideProps(overrides, "View")}
+    >
+      <Image
+        width="1554px"
+        height="1189px"
+        position="absolute"
+        top="-91px"
+        left="-94px"
+        padding="0px 0px 0px 0px"
+        {...getOverrideProps(overrides, "View.Image[0]")}
+      ></Image>
+      <View
+        padding="0px 0px 0px 0px"
+        width="149px"
+        height="37px"
+        position="absolute"
+        top="612px"
+        left="696px"
+        {...getOverrideProps(overrides, "View.View[0]")}
+      >
+        <View
+          width="149px"
+          height="37px"
+          position="absolute"
+          top="0px"
+          left="0px"
+          borderRadius="55px"
+          padding="0px 0px 0px 0px"
+          backgroundColor="rgba(255,236.77265167236328,190.191650390625,1)"
+          {...getOverrideProps(overrides, "View.View[0].View[0]")}
+        ></View>
+      </View>
+      <View
+        width="1366px"
+        height="164px"
+        position="absolute"
+        top="843px"
+        left="0px"
+        padding="0px 0px 0px 0px"
+        backgroundColor="rgba(255,255,255,1)"
+        {...getOverrideProps(overrides, "View.View[1]")}
+      ></View>
+      <Text
+        fontFamily="Gotham"
+        fontSize="14px"
+        fontWeight="500"
+        color="rgba(0,0,0,1)"
+        textTransform="uppercase"
+        lineHeight="14px"
+        textAlign="center"
+        display="flex"
+        direction="column"
+        justifyContent="flex-start"
+        width="101px"
+        height="21px"
+        position="absolute"
+        top="625px"
+        left="721px"
+        padding="0px 0px 0px 0px"
+        children="Register"
+        {...getOverrideProps(overrides, "View.Text[0]")}
+      ></Text>
+      <Text
+        fontFamily="Gotham"
+        fontSize="14px"
+        fontWeight="500"
+        color="rgba(0,0,0,1)"
+        textTransform="uppercase"
+        lineHeight="14px"
+        textAlign="left"
+        display="flex"
+        direction="column"
+        justifyContent="flex-start"
+        width="200px"
+        height="21px"
+        position="absolute"
+        top="625px"
+        left="521px"
+        padding="0px 0px 0px 0px"
+        children="Forgot password?"
+        {...getOverrideProps(overrides, "View.Text[1]")}
+      ></Text>
+      <View
+        padding="0px 0px 0px 0px"
+        width="1286px"
+        height="52px"
+        position="absolute"
+        top="884px"
+        left="40px"
+        {...getOverrideProps(overrides, "View.View[2]")}
+      >
+        <View
+          width="1286px"
+          height="1px"
+          position="absolute"
+          top="0px"
+          left="0px"
+          padding="0px 0px 0px 0px"
+          backgroundColor="rgba(214.03564453125,214.03564453125,214.03564453125,1)"
+          {...getOverrideProps(overrides, "View.View[2].View[0]")}
+        ></View>
+        <Text
+          fontFamily="Gotham"
+          fontSize="14px"
+          fontWeight="500"
+          color="rgba(255,200.00000327825546,30.00000011175871,1)"
+          lineHeight="14px"
+          textAlign="left"
+          display="flex"
+          direction="column"
+          justifyContent="flex-start"
+          letterSpacing="0.41000000000000003px"
+          width="689px"
+          height="19px"
+          position="absolute"
+          top="31px"
+          left="0px"
+          padding="0px 0px 0px 0px"
+          children="Help       Legal       Privacy       Cookies&#xA;"
+          {...getOverrideProps(overrides, "View.View[2].Text[0]")}
+        ></Text>
+        <Text
+          fontFamily="Gotham"
+          fontSize="14px"
+          fontWeight="700"
+          color="rgba(0,0,0,1)"
+          lineHeight="14px"
+          textAlign="right"
+          display="flex"
+          direction="column"
+          justifyContent="flex-start"
+          letterSpacing="0.41000000000000003px"
+          width="160px"
+          height="19px"
+          position="absolute"
+          top="31px"
+          left="1126px"
+          padding="0px 0px 0px 0px"
+          children="© 2021 frtyfve&#xA;"
+          {...getOverrideProps(overrides, "View.View[2].Text[1]")}
+        ></Text>
+        <View
+          width="35px"
+          height="2px"
+          position="absolute"
+          top="50px"
+          left="0px"
+          padding="0px 0px 0px 0px"
+          backgroundImage="linear-gradient(88deg, rgba(255,200.00000327825546,30.00000011175871,1), rgba(255,200.00000327825546,30.00000011175871,1))"
+          {...getOverrideProps(overrides, "View.View[2].View[1]")}
+        ></View>
+      </View>
+      <View
+        padding="0px 0px 0px 0px"
+        width="178px"
+        height="45px"
+        position="absolute"
+        top="497px"
+        left="594px"
+        {...getOverrideProps(overrides, "View.View[3]")}
+      >
+        <View
+          width="178px"
+          height="45px"
+          position="absolute"
+          top="0px"
+          left="0px"
+          borderRadius="55px"
+          padding="0px 0px 0px 0px"
+          backgroundColor="rgba(0,0,0,1)"
+          {...getOverrideProps(overrides, "View.View[3].View[0]")}
+        ></View>
+        <Text
+          fontFamily="Gotham"
+          fontSize="14px"
+          fontWeight="700"
+          color="rgba(255,255,255,1)"
+          textTransform="uppercase"
+          lineHeight="16.65999984741211px"
+          textAlign="center"
+          display="flex"
+          direction="column"
+          justifyContent="flex-start"
+          width="118px"
+          height="19px"
+          position="absolute"
+          top="14px"
+          left="30px"
+          padding="0px 0px 0px 0px"
+          children="LOGIN"
+          {...getOverrideProps(overrides, "View.View[3].Text[0]")}
+        ></Text>
+      </View>
+      <Text
+        fontFamily="Gotham"
+        fontSize="56px"
+        fontWeight="700"
+        color="rgba(0,0,0,1)"
+        lineHeight="56px"
+        textAlign="center"
+        display="flex"
+        direction="column"
+        justifyContent="flex-start"
+        width="432px"
+        height="71px"
+        position="absolute"
+        top="269px"
+        left="467px"
+        padding="0px 0px 0px 0px"
+        children="Welcome"
+        {...getOverrideProps(overrides, "View.Text[2]")}
+      ></Text>
+      <View
+        padding="0px 0px 0px 0px"
+        width="324px"
+        height="48px"
+        position="absolute"
+        top="367px"
+        left="521px"
+        {...getOverrideProps(overrides, "View.View[4]")}
+      >
+        <View
+          width="324px"
+          height="48px"
+          position="absolute"
+          top="0px"
+          left="0px"
+          border="1px SOLID rgba(204.00000303983688,204.00000303983688,204.00000303983688,1)"
+          borderRadius="6px"
+          padding="0px 0px 0px 0px"
+          backgroundColor="rgba(255,255,255,0.8799999952316284)"
+          {...getOverrideProps(overrides, "View.View[4].View[0]")}
+        ></View>
+        <View
+          padding="0px 0px 0px 0px"
+          width="224.1203155517578px"
+          height="21px"
+          position="absolute"
+          top="19px"
+          left="21.920352935791016px"
+          {...getOverrideProps(overrides, "View.View[4].View[1]")}
+        >
+          <Text
+            fontFamily="Gotham"
+            fontSize="14px"
+            fontWeight="500"
+            color="rgba(0,0,0,1)"
+            lineHeight="14px"
+            textAlign="left"
+            display="flex"
+            direction="column"
+            justifyContent="flex-start"
+            width="224.1203155517578px"
+            height="21px"
+            position="absolute"
+            top="0px"
+            left="0px"
+            padding="0px 0px 0px 0px"
+            children="User"
+            {...getOverrideProps(overrides, "View.View[4].View[1].Text[0]")}
+          ></Text>
+        </View>
+      </View>
+      <View
+        padding="0px 0px 0px 0px"
+        width="324px"
+        height="48px"
+        position="absolute"
+        top="422px"
+        left="521px"
+        {...getOverrideProps(overrides, "View.View[5]")}
+      >
+        <View
+          width="324px"
+          height="48px"
+          position="absolute"
+          top="0px"
+          left="0px"
+          border="2px SOLID rgba(255,41.00000135600567,87.00000241398811,1)"
+          borderRadius="6px"
+          padding="0px 0px 0px 0px"
+          backgroundColor="rgba(255,255,255,0.8799999952316284)"
+          {...getOverrideProps(overrides, "View.View[5].View[0]")}
+        ></View>
+        <View
+          padding="0px 0px 0px 0px"
+          width="224.1203155517578px"
+          height="21px"
+          position="absolute"
+          top="18px"
+          left="21.920352935791016px"
+          {...getOverrideProps(overrides, "View.View[5].View[1]")}
+        >
+          <Text
+            fontFamily="Gotham"
+            fontSize="14px"
+            fontWeight="500"
+            color="rgba(0,0,0,1)"
+            lineHeight="14px"
+            textAlign="left"
+            display="flex"
+            direction="column"
+            justifyContent="flex-start"
+            width="224.1203155517578px"
+            height="21px"
+            position="absolute"
+            top="0px"
+            left="0px"
+            padding="0px 0px 0px 0px"
+            children="•••••••••••••••••••"
+            {...getOverrideProps(overrides, "View.View[5].View[1].Text[0]")}
+          ></Text>
+        </View>
+      </View>
+      <Text
+        fontFamily="Gotham"
+        fontSize="36px"
+        fontWeight="700"
+        color="rgba(0,0,0,1)"
+        lineHeight="36px"
+        textAlign="left"
+        display="flex"
+        direction="column"
+        justifyContent="flex-start"
+        letterSpacing="1.29px"
+        width="664px"
+        height="64px"
+        position="absolute"
+        top="54px"
+        left="40px"
+        padding="0px 0px 0px 0px"
+        children="frtyfve"
+        {...getOverrideProps(overrides, "View.Text[3]")}
+      ></Text>
+    </View>
+  );
+}
