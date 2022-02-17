600a032f471f459a2a954cde3a3eba41faf78df0
Kevin Ikelle <kevin.ikelle@weareinstrumental.com>
2022-02-17 16:21:53 +0000

    rollbvack

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
-import { Amplify } from 'aws-amplify';
-
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Amplify } from 'aws-amplify'
 import { withAuthenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 
 import awsExports from './aws-exports';
-Amplify.configure(awsExports);
 
-function App({ signOut, user }) {
+Amplify.configure(awsExports);
+function App() {
   return (
-    <>
-      <h1>Hello {user.username}</h1>
-      <button onClick={signOut}>Sign out</button>
-    </>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Hello from V2</h1>
+      </header>
+    </div>
   );
 }
 
-export default withAuthenticator(App);
+export default App;
